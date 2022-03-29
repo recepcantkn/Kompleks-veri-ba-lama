de2b10cd31dcd8a9e0c066ad7b3e65f39d5a3300
Recep Can <61096668+recepcantkn@users.noreply.github.com>
2022-03-29 09:37:06 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication33
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVeriBagla_Click(object sender, EventArgs e)
+        {
+            string[] diller = { "C#", "Java", "Python","Delphi"};
+            listeVeriler.DataSource = diller;
+        }
+    }
+}
